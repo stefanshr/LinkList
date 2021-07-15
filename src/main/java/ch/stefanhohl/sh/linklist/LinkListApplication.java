313e2e7d9644bd70515655dea3b7e82671c6df51
stefanshr <stefan@familie-hohl.ch>
2021-07-15 08:42:00 +0200

    First Commit

---
+++ src/main/java/ch/stefanhohl/sh/linklist/LinkListApplication.java
@@ -0,0 +1,13 @@
+package ch.stefanhohl.sh.linklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkListApplication.class, args);
+    }
+
+}
